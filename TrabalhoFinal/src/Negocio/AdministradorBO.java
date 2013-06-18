647e84466cec62e0d464211361abd0aa3a0d8ba4
Iara <Iara@validation.sls.microsoft.com>
2013-06-18 19:31:48 -0300

    novas classes DAO

---
+++ TrabalhoFinal/src/Negocio/AdministradorBO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import DataAccess.AdministradorDAO;
+import DomainModel.Administrador;
+
+/**
+ *
+ * @author Iara
+ */
+public class AdministradorBO {
+    private AdministradorDAO administradorDAO = new AdministradorDAO();
+    
+    public boolean Salvar(Administrador administrador){
+        if(administrador.getUsuario().trim().length() == 0){
+            throw new RuntimeException("O usuario nao pode ser vazio!");
+        }
+        if (administrador.getUsuario().length() > 20){
+            throw new RuntimeException("O usuario esta além do tamanho permitido!");
+        }
+        administradorDAO.Salvar(administrador);
+        return true;
+    }
+    public Administrador Abrir(int codigo){
+        return administradorDAO.Abrir(codigo);
+    }
+    public void Apagar(Administrador administrador){
+        administradorDAO.Apagar(administrador);
+    }
+    
+}
